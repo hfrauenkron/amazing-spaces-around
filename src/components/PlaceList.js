faa61912a804cf5134321aad2e3ecb12adb421ed
Hanna Frauenkron <h.frauenkron@mail.de>
2019-09-11 12:10:06 +0200

    add placeholder components

---
+++ src/components/PlaceList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Place from "./Place";
+
+function PlaceList() {
+  return (
+    <div>
+      PlaceList
+      <Place />
+    </div>
+  );
+}
+
+export default PlaceList;
